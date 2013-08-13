846da2bb0965fb9951f3dce0f3a6840b71960825
victortxu <victortxu@gmail.com>
2013-08-13 18:30:26 +0000

    modificat web.js per HW2

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+ var fs = require('fs');
+
+ response.send(buffer.toString('utf-8',fs.readFileSync("index.html")));
+
 });
 
 var port = process.env.PORT || 5000;
